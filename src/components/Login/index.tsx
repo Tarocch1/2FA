edbd9ef174dd8f922c4bfe47e72332902b088c3d
Tarocch1 <huiben.fang@gmail.com>
2020-01-15 09:37:37 +0800

    调整登录页样式

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Row, Col, Button } from 'antd';
+import { Row, Col, Result, Button } from 'antd';
 import { oauth_base, api } from '../../constants/api';
 
 function Login() {
   return (
     <Row style={{ height: '100%' }} type="flex" justify="center" align="middle">
       <Col>
-        <Button
-          icon="aliyun"
-          href={`${oauth_base}${api.login}?redirect=${window.location.origin}${window.location.pathname}`}
-        >
-          使用阿里云登录
-        </Button>
+        <Result
+          status="403"
+          title="403"
+          extra={
+            <Button
+              type="primary"
+              href={`${oauth_base}${api.login}?redirect=${window.location.origin}${window.location.pathname}`}
+            >
+              登录
+            </Button>
+          }
+        />
       </Col>
     </Row>
   );
