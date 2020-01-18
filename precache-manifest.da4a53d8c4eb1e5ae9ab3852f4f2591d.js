4edc383114fe8d46a4b6e5d59850a16f447353bc
Tarocch1 <huiben.fang@gmail.com>
2020-01-18 09:59:59 +0000

    Deploy by Github Actions

--- precache-manifest.da4a53d8c4eb1e5ae9ab3852f4f2591d.js
+++ precache-manifest.da4a53d8c4eb1e5ae9ab3852f4f2591d.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "806b3de7f7b8f8332204d34200f81adc",
+    "revision": "73be64b1f831df981abe1825c380172b",
     "url": "/2FA/index.html"
   },
   {
-    "revision": "8e0195979b1e319aa1e7",
+    "revision": "afc42c8e5961288270b0",
     "url": "/2FA/static/css/2.ff8bd605.chunk.css"
   },
   {
-    "revision": "e19f9008472b37578b00",
+    "revision": "249b9c8a878d1381abd7",
     "url": "/2FA/static/css/main.5e06658f.chunk.css"
   },
   {
-    "revision": "8e0195979b1e319aa1e7",
-    "url": "/2FA/static/js/2.15bbb020.chunk.js"
+    "revision": "afc42c8e5961288270b0",
+    "url": "/2FA/static/js/2.b5567b57.chunk.js"
   },
   {
     "revision": "437b46b5c51c931d0145bcf1e7996377",
-    "url": "/2FA/static/js/2.15bbb020.chunk.js.LICENSE"
+    "url": "/2FA/static/js/2.b5567b57.chunk.js.LICENSE"
   },
   {
-    "revision": "e19f9008472b37578b00",
-    "url": "/2FA/static/js/main.118ccfce.chunk.js"
+    "revision": "249b9c8a878d1381abd7",
+    "url": "/2FA/static/js/main.0985690a.chunk.js"
   },
   {
     "revision": "1cbc09688507f1eeeac0",
