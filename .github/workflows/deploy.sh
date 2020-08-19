249187235d4efe6ed773c681a692c09cbb98b649
Tarocch1 <huiben.fang@gmail.com>
2020-08-19 10:11:31 +0800

    Update deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,8 +1,8 @@
 git clone "https://Tarocch1:${GITHUB_TOKEN}@github.com/Tarocch1/MFA.git" -b gh-pages tmp
 cp -r tmp/.git build/
 cd build
-git config user.name "Tarocch1"
-git config user.email "huiben.fang@gmail.com"
+git config user.name "github-actions[bot]"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git status
 git add -A
 if [ $? -eq 0 ]
