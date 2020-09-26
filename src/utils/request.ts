5cda2a914d482e6531c9cb893f1640f323af7f6c
Tarocch1 <huiben.fang@gmail.com>
2020-09-26 15:02:20 +0800

    删除旧的请求认证头

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,9 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 export default function request(options: AxiosRequestConfig) {
-  const jwt = JSON.parse(window.localStorage.getItem('jwt') || '{}');
-  const token = jwt.token || '';
-  if (token) axios.defaults.headers.common['Token'] = token;
   return axios(options)
     .then(res => res.data)
     .catch(error => {
